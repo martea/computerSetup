949c515abba1d810e80c66a8207462d677e076d2
Mårten Andersson <maarten.andersson@gmail.com>
2017-07-20 08:44:25 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,5 +1,8 @@
 iex ((new-object net.webclient).DownloadString('http://bit.ly/psChocInstall'))
 
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 .\install-chocolatey.ps1
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 .\install-webpi-essentials.ps1
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 .\install-vscode-extensions.ps1
